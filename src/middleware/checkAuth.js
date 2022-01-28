5a7fffc3c41cafec5673e7ccfa10883e3679ace6
Kirill <k.pirinen.rus@gmail.com>
2022-01-29 00:32:03 +0300

    middleware changed

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -1,7 +1,7 @@
 const appError = require('../Errors/errors');
 
 const checkAuth = (req, res, next) => {
-  if (!req.session.user.id) {
+  if (!req.session.user) {
     return next(new appError(401, 'Доступно только для авторизированных пользователей. Авторизируйтесь или зарегистрируйтесь'))
   } else {
     return next();
